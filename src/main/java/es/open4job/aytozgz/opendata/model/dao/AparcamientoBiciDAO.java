b94da093342b97283a2ab2d366789410c857a871
AitorPrat <AitorPrat@gmail.com>
2015-01-27 11:06:18 +0100

    clase AparcamientoBiciDAO

---
+++ src/main/java/es/open4job/aytozgz/opendata/model/dao/AparcamientoBiciDAO.java
@@ -0,0 +1,21 @@
+package es.open4job.aytozgz.opendata.model.dao;
+
+import java.util.List;
+
+import es.open4job.aytozgz.opendata.model.vo.AparcamientoBiciVO;
+
+public class AparcamientoBiciDAO {
+
+	public List<AparcamientoBiciVO> getListadoAparcamientoBici() {
+	
+		return null;
+		
+	}
+	
+	public AparcamientoBiciVO getDatosAparcamientoBici (int idAparcamientoBici) {
+	
+		return null;
+		
+	}
+	
+}
