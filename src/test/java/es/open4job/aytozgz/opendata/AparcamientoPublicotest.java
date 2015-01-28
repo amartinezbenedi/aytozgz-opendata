553d59967c44a2716f134c1fd25c701132f08618
amartinezbenedi <andrea.martinez.benedi@gmail.com>
2015-01-28 11:37:51 +0100

    Añadida Clase de test AparcamientoPublicoTest

---
+++ src/test/java/es/open4job/aytozgz/opendata/AparcamientoPublicotest.java
@@ -0,0 +1,39 @@
+package es.open4job.aytozgz.opendata;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.open4job.aytozgz.opendata.model.dao.AparcamientosPublicosDAO;
+import es.open4job.aytozgz.opendata.model.vo.AparcamientosPublicosVO;
+
+public class AparcamientoPublicotest {
+	
+	AparcamientosPublicosDAO apDAO = new AparcamientosPublicosDAO();
+
+	public AparcamientoPublicotest() {
+	}
+
+	@Before
+	public void setUp() throws Exception {		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetListadoAparcamientos() {
+		
+		List<AparcamientosPublicosVO> aparcamientos = new ArrayList<AparcamientosPublicosVO>();
+		aparcamientos = apDAO.getListadoAparcamientos();
+		assertNotNull(aparcamientos);
+		
+	}
+
+}
