b4ab41d75141c618ab16bee99e2100f9071592b6
AitorPrat <AitorPrat@gmail.com>
2015-01-28 11:01:22 +0100

    Prueba

--- src/main/java/es/open4job/aytozgz/opendata/App.java
+++ src/main/java/es/open4job/aytozgz/opendata/App.java
@@ -8,21 +8,8 @@ import es.open4job.aytozgz.opendata.model.vo.AparcamientoBiciVO;
 
 public class App {
 	
-    public static void main( String[] args )
-    {
-        ConsultaOrcl objeto= new ConsultaOrcl();
-        
-<<<<<<< HEAD
-        String consulta= "SELECT * FROM EQ4_BICI";
-        String resp= objeto.consultaOrcl(consulta, 2);
-        
-        System.out.println(resp);
-        
-               
-=======
-                
->>>>>>> branch 'master' of https://github.com/amartinezbenedi/aytozgz-opendata
-    }
+    public static void main( String[] args ){
 
+    }
 }
 
