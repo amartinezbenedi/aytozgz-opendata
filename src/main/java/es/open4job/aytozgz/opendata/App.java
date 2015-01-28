b958d6c06df6e8c58a114fc9d30d4e85e484ccc8
amartinezbenedi <andrea.martinez.benedi@gmail.com>
2015-01-28 13:37:51 +0100

    Prueba

--- src/main/java/es/open4job/aytozgz/opendata/App.java
+++ src/main/java/es/open4job/aytozgz/opendata/App.java
@@ -4,7 +4,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import es.open4job.aytozgz.opendata.model.dao.AparcamientoBiciDAO;
+import es.open4job.aytozgz.opendata.model.dao.AparcamientosPublicosDAO;
 import es.open4job.aytozgz.opendata.model.vo.AparcamientoBiciVO;
+import es.open4job.aytozgz.opendata.model.vo.AparcamientosPublicosVO;
 
 public class App {
 	
@@ -12,6 +14,8 @@ public class App {
    	
     	AparcamientoBiciDAO bicisDAO = new AparcamientoBiciDAO();
     	List<AparcamientoBiciVO> bicis = bicisDAO.getListadoAparcamientoBici();
+    	AparcamientosPublicosDAO apDAO = new AparcamientosPublicosDAO();
+    	List<AparcamientosPublicosVO> aparcamientos = apDAO.getListadoAparcamientos();
     	System.out.print("");  			
     			
     	if (bicis != null) {
@@ -19,6 +23,13 @@ public class App {
     		while (iterator.hasNext()) {
     			AparcamientoBiciVO bici = iterator.next();
     		System.out.println(bici.toString());
+    		}
+    	}	
+    	if (aparcamientos !=null){
+    	Iterator<AparcamientosPublicosVO> it = aparcamientos.iterator();
+    		while (it.hasNext()){
+    			AparcamientosPublicosVO aparcamiento = it.next();
+    		System.out.println(aparcamiento.toString());
     		}
     	}
     }
