1199fa9738b668145b51f900b802b2f6f133dc7d
AitorPrat <AitorPrat@gmail.com>
2015-01-28 09:33:10 +0100

    .

--- src/main/java/es/open4job/aytozgz/opendata/App.java
+++ src/main/java/es/open4job/aytozgz/opendata/App.java
@@ -1,13 +1,32 @@
 package es.open4job.aytozgz.opendata;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello aaaa " );
+import java.sql.SQLException;
+
+import es.open4job.aytozgz.opendata.model.dao.AparcamientoBiciDAO;
+import es.open4job.aytozgz.opendata.model.vo.AparcamientoBiciVO;
+
+public class App {
+    public static void main( String[] args ){
+
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String host = "54.154.192.80";
+		String puerto = "1521";
+		String sid = "xe";
+		String user = "test";
+		String password = "test";
+		String url = "jdbc:oracle:thin:" + user + "/" + password + "@" + host
+		+ ":" + puerto + ":" + sid;
+		
+		AparcamientoBiciDAO aparcamientoBiciDAO = new AparcamientoBiciDAO(driver,
+				url, user, password);
+		
+		/*try {
+			gastoCorrienteDAO.prueba();
+			} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}*/
     }
 }
