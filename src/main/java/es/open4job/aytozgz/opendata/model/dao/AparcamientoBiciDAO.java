5aef6f224188c6570e9df6a61209c8d7ba04c9d5
AitorPrat <AitorPrat@gmail.com>
2015-01-28 09:33:38 +0100

    .

--- src/main/java/es/open4job/aytozgz/opendata/model/dao/AparcamientoBiciDAO.java
+++ src/main/java/es/open4job/aytozgz/opendata/model/dao/AparcamientoBiciDAO.java
@@ -1,21 +1,37 @@
 package es.open4job.aytozgz.opendata.model.dao;
 
+import java.sql.*;
 import java.util.List;
 
 import es.open4job.aytozgz.opendata.model.vo.AparcamientoBiciVO;
 
-public class AparcamientoBiciDAO {
+public class AparcamientoBiciDAO extends AbstractDAO{
 
+	public AparcamientoBiciDAO(String driver, String url, String user,
+			String password) {
+		super(driver, url, user, password);		
+	}
+	
+	// Listado de aparcamiento bicis
 	public List<AparcamientoBiciVO> getListadoAparcamientoBici() {
+		List<AparcamientoBiciVO> bicis = null;
+		ResultSet result = null;
+		try {
+			result = connection.createStatement().executeQuery(
+					"SELECT 'hola mundo' FROM DUAL");
+			result.next();
+			System.out.println(result.getString(1));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return bicis;
+	}
+
 	
+	/*public AparcamientoBiciVO getDatosAparcamientoBici (int id) {
+		
 		return null;
 		
-	}
-	
-	public AparcamientoBiciVO getDatosAparcamientoBici (int idAparcamientoBici) {
-	
-		return null;
-		
-	}
+	}*/
 	
 }
